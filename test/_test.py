604f492d13f5c4ee0b1384fd8a21bd73158b9dab
shahar azulay <shahar4@gmail.com>
2018-08-28 14:13:38 +0300

    Update _test.py

--- test/_test.py
+++ test/_test.py
@@ -1,10 +1,10 @@
 import unittest
 
-from traceable_dict._utils import key_removed, key_added, key_updated
-from traceable_dict._diff import root
-
 from traceable_dict import DictDiff
 from traceable_dict import TraceableDict
+
+from traceable_dict._utils import key_removed, key_added, key_updated
+from traceable_dict._diff import root
 
 
 class KeyEventTypeTests(unittest.TestCase):
